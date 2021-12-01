11cece0a3e621d83c7b93d7cdb9f9f5c6e8a21e1
Tom Voet <tomvoet@gmx.de>
2021-12-01 22:08:05 +0100

    Setup

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <webcamVideo />
+</template>
+
+<script>
+import webcamVideo from '@/components/video.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    webcamVideo
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  height: 100vh;
+  width: 100vw;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+}
+
+</style>
---
+++ src/components/video.vue
@@ -0,0 +1,29 @@
+<template>
+    <video autoplay="true" id="video"></video>
+</template>
+
+<script>
+export default {
+  name: 'webcamVideo',
+  mounted() {
+    var video = document.querySelector("#video");
+
+    if (navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({
+        video: true
+      }).then(
+        stream => (video.srcObject = stream),
+        err => console.log(err)
+      );
+    }
+  }
+}
+</script>
+
+<style scoped>
+    #video {
+        width: 100vw;
+        height: 100vh;
+        transform: scaleX(-1)
+    }
+</style>
