985ff76247c2287255a99aeb1cbfda4a938ff2ac
Tom Voet <tomvoet@gmx.de>
2021-12-05 20:46:19 +0100

    tryed out auto calendar fetch

--- src/main.js
+++ src/main.js
@@ -3,7 +3,17 @@ import App from './App.vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import ical from 'node-ical'
+//import fs from "fs"
+/*
+import keys from "../keys.json"
+console.log("res")
 
+setInterval(() => {
+    axios.get(keys.calendarLocation).then((res) => {
+        console.log(res)
+    })
+}, 5500)
+*/
 const app = createApp(App).use(VueAxios, axios, ical)
 app.provide('axios', app.config.globalProperties.axios)
 app.mount('#app')
