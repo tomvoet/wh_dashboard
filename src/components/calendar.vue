9e0b22a92230a4af84a10cede4d56de2a87fe335
Tom Voet <tomvoet@gmx.de>
2021-12-09 21:25:37 +0100

    calendar styling

--- src/components/calendar.vue
+++ src/components/calendar.vue
@@ -1,19 +1,25 @@
 <template>
   <div id="calendarContainer">
-    <div class="event" v-for="event of this.events" :key="event">
+    <div id="calendarWrapper">
+    <div class="event" v-for="event of this.events" :key="event" :style="{backgroundColor: this.colors[this.events.indexOf(event) % this.colors.length] }">
       <div class="date">
         <div>
-        {{ event.start.getDate().toString() + "." }}
-        </div>
-        <!-- https://mythemeshop.com/wp-content/uploads/2020/01/EventON-.png -->
-        <div> 
-          {{ (event.start.getMonth() + 1).toString() + "." }}
-        </div>
-        <div>
-          {{ this.weekday[event.start.getDay()] }}
+          <div>
+            {{ this.weekday[event.start.getDay()] }} 
+          </div>
+          <!-- https://mythemeshop.com/wp-content/uploads/2020/01/EventON-.png -->
+          <div style="font-size: 1.5em; font-weight: 900;"> 
+            {{ event.start.getDate().toString() + "." }}
+          </div>
+          <div>
+            {{ this.months[event.start.getMonth()] }}
+          </div>
         </div>
       </div>
-      {{event.summary}}
+      <div class="dateText">
+      <span>{{event.summary}}</span>
+      </div>
+    </div>
     </div>
   </div>
 </template>
@@ -27,7 +33,9 @@ export default {
   data() {
     return {
       events: [],
-      weekday: ["So","Mo","Di","Mi","Do","Fr","Sa"]
+      weekday: ["So","Mo","Di","Mi","Do","Fr","Sa"],
+      colors: ["#17EBA0", "#F740FF", "#00C8FF", "#FC6161", "#FFBC44", "#FFEB59"],
+      months: ["Jan.", "Febr.", "März", "Apr.", "Mai", "Juni", "Juli", "Aug.", "Sep.", "Okt.", "Nov.", "Dez."]
     };
   },
   methods: {
@@ -68,21 +76,55 @@ export default {
       setInterval(() => {
         this.parseCalendar();
       }, 5000 * 60);
+      /*setInterval(() => {
+
+      }, )*/
     });
   },
 };
 </script>
 
 <style scoped>
-div {
-  color: white;
-}
 .date {
-  width: 10%;
+  width: 14%;
   float: left;
+  padding: auto 0;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  padding-top: 4px;
 }
+
+.date > div > div {
+  width: max-content;
+  margin: 0 auto;
+}
+
+
 .event {
   display: flex;
   flex-direction: row;
+  color: rgb(36, 36, 36);
+}
+
+.dateText {
+  flex: 1;
+  padding: 1em 1em 1em .1em;
+  font-size: 1.5em;
+  font-weight: 600;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+
+.dateText > span {
+  margin-top: .1em;
+}
+
+#calendarWrapper {
+  overflow: hidden;
+  height: 100%;
+  width: 100%;
 }
 </style>
